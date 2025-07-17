33858f28f58b4279cdb7cca2cbcda23fbb10b170
Savirean07 <jangidhimanshu47@gmail.com>
2025-07-17 17:40:47 +0530

    Jenkins

--- run-owasp.sh
+++ run-owasp.sh
@@ -22,7 +22,8 @@ fi
 
 DC_EXECUTABLE="${DC_DIR}/bin/dependency-check.sh"
 
-DC_ARGS="--project GITOPS-CI-CD-IMP --scan . --format ALL --out ./owasp-report --disableYarnAudit --noupdate --enableExperimental --disableArchive"
+mkdir -p odc-data
+DC_ARGS="--project GITOPS-CI-CD-IMP --scan . --format ALL --out ./owasp-report --data ./odc-data --disableYarnAudit --noupdate --enableExperimental --disableArchive"
 
 if [ -n "$NVD_API_KEY" ]; then
   DC_ARGS="$DC_ARGS --nvdApiKey $NVD_API_KEY"
